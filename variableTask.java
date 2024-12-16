9727fc50d3e31c3dd998d893ee67fe80200f5dc8
mohamirfaan <165769643+mohamirfaan@users.noreply.github.com>
2024-12-16 17:04:39 +0530

    day 01-variable task

---
+++ variableTask.java
@@ -0,0 +1,26 @@
+public class variableTask {
+    public static void main(String[] args) {
+        
+        byte byteVar = 100;
+        short shortVar = 32000;
+        int intVar = 100000;
+        long longVar = 10000000000L;
+        float floatVar = 3.14f;
+        double doubleVar = 12345.6789;
+        char charVar = 'A';
+        boolean booleanVar = true;
+
+        
+        System.out.println("byte: " + byteVar);
+        System.out.println("short: " + shortVar);
+        System.out.println("int: " + intVar);
+        System.out.println("long: " + longVar);
+        System.out.println("float: " + floatVar);
+        System.out.println("double: " + doubleVar);
+        System.out.println("char: " + charVar);
+        System.out.println("boolean: " + booleanVar);
+    }
+}
+ 
+    
+
